49bb81419c3bd08c84511c2c184c1decee317903
John Andrey <andrey_201015@hotmail.com>
2019-07-26 21:50:14 -0500

    Add project styles

--- pages/portafolio.js
+++ pages/portafolio.js
@@ -20,9 +20,24 @@ class Portafolio extends Component {
       <Layout {...SEO}>
         <section id='Portafolio'>
           <ul className='projects'>
-            <li className='project'>Project 1</li>
-            <li className='project'>Project 2</li>
-            <li className='project'>Project 3</li>
+            <li className='project'>
+              <div className='caption light'>
+                <h3>Paila</h3>
+                <span>Branding, Mobile, UX/UI</span>
+              </div>
+            </li>
+            <li className='project'>
+              <div className='caption light'>
+                <h3>Paila</h3>
+                <span>Branding, Mobile, UX/UI</span>
+              </div>
+            </li>
+            <li className='project'>
+              <div className='caption light'>
+                <h3>Paila</h3>
+                <span>Branding, Mobile, UX/UI</span>
+              </div>
+            </li>
           </ul>
         </section>
         <style jsx>{`
@@ -35,6 +50,52 @@ class Portafolio extends Component {
             list-style: none;
             padding: 0;
             margin: 0;
+          }
+
+          .project {
+            background: url('/static/project.png') no-repeat;
+            padding-bottom: 56.25%;
+            position: relative;
+            overflow: hidden;
+          }
+          .project:before {
+            content: "";
+            display: block;
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            z-index: 8;
+            transition: transform 200ms linear;
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-position: center center;
+            background-image: inherit;
+          }
+          .project:hover:before {
+            transform: scale(1.1);
+          }
+          .light {
+            color: #ffffff;
+          }
+          .caption {
+            position: absolute;
+            left: 8%;
+            bottom: 0%;
+            padding-bottom: 7%;
+            transition: bottom 300ms linear;
+            z-index: 9;
+          }
+          .caption h3 {
+            font-size: 1.875rem;
+            line-height: 2.25rem;
+            margin-bottom: 10px;
+          }
+          .caption span {
+            font-size: 0.8rem;
+            line-height: 1rem;
+            text-transform: uppercase;
           }
         `}</style>
       </Layout>
