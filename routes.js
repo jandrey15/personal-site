224636efbd3e91e80bcab8e3f497eb6c1a760565
John Serrano <andrey_201015@hotmail.com>
2019-09-03 09:19:17 -0500

    Changes redirect routes express

--- routes.js
+++ routes.js
@@ -10,12 +10,18 @@ const router = express.Router()
 // })
 
 router.get('\\S+\/$', function (req, res) {
-  return res.redirect(301, req.path.slice(0, -1) + req.url.slice(req.path.length))
+  console.log(req.path.slice(0, -1))
+  console.log('------------------------')
+  console.log(req.url.slice(req.path.length))
+  res.redirect(301, req.path.slice(0, -1) + req.url.slice(req.path.length))
 })
 
-router.get('/blog/', (req, res) => {
-  return app.render(req, res, '/blog')
-})
+// router.get('/blog/', (req, res) => {
+//   // return app.render(req, res, '/blog')
+//   // const targetUrl = targetBaseUrl + req.originalUrl;
+//   console.log(req.originalUrl)
+//   res.redirect(301, 'https://johnserrano.co/blog')
+// })
 
 router.get('/blog/:slug', (req, res) => {
   return app.render(req, res, '/post', { slug: req.params.slug })
