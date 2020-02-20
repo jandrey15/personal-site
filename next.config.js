213e9c495f3ba3844a63b7c5bd9995f083328308
John Serrano <andrey_201015@hotmail.com>
2020-02-20 10:47:04 -0500

    Changes now config

--- next.config.js
+++ next.config.js
@@ -2,8 +2,34 @@ require('dotenv').config()
 
 const path = require('path')
 const Dotenv = require('dotenv-webpack')
+const withOffline = require('next-offline')
 
-module.exports = {
+const nextConfig = {
+  target: 'serverless',
+  transformManifest: manifest => ['/'].concat(manifest), // add the homepage to the cache
+  // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
+  // turn on the SW in dev mode so that we can actually test it
+  generateInDevMode: true,
+  workboxOpts: {
+    swDest: 'static/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'https-calls',
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
+          },
+          cacheableResponse: {
+            statuses: [0, 200]
+          }
+        }
+      }
+    ]
+  },
   webpack: config => {
     config.plugins = config.plugins || []
 
@@ -20,3 +46,5 @@ module.exports = {
     return config
   }
 }
+
+module.exports = withOffline(nextConfig)
