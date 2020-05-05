181f5816eab63910e30be0815217eda02ba5cbdd
John Andrey <andrey_201015@hotmail.com>
2020-05-05 17:59:31 -0500

    Change key algolia

--- helpers/algolia.js
+++ helpers/algolia.js
@@ -3,6 +3,6 @@ import algoliasearch from 'algoliasearch/lite'
 const API_KEY_ALGOLIA = process.env.API_KEY_ALGOLIA
 // Config algolia
 const client = algoliasearch('ZKVB80BMVH', API_KEY_ALGOLIA)
-const index = client.initIndex('instant_search')
+const index = client.initIndex('instant_search_prod')
 
 export default index
