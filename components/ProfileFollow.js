3858f1e19ff8d70f0a7c1450c23f4aa12d6e4836
John Serrano <andrey_201015@hotmail.com>
2020-09-13 16:12:34 -0500

    Changes manifest and vercel

--- components/ProfileFollow.js
+++ components/ProfileFollow.js
@@ -7,6 +7,7 @@ const ProfileFollow = () => {
       <ul className='follow__buttons'>
         <li><a href='https://www.facebook.com/johnserrano15' title='Facebook' target='_blank'><img alt='en Facebook' src='/follow/Facebook.svg' /></a></li>
         <li><a href='https://twitter.com/jandrey15' target='_blank' title='Twitter'><img alt='Twitter' src='/follow/Twitter.svg' /></a></li>
+        <li><a href='https://github.com/johnsi15' target='_blank' title='GitHub'><img alt='GitHub' src='/follow/Github.svg' /></a></li>
         <li><a href='https://www.linkedin.com/in/jandreys15' target='_blank' title='LinkedIn' ><img alt='LinkedIn' src='/follow/LinkedIn.svg' /></a></li>
       </ul>
       <style jsx>{`
