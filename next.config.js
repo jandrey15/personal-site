3dfe232987c152812185d885dc73a053b069fcbf
John Serrano <andrey_201015@hotmail.com>
2020-09-13 18:31:32 -0500

    Changes start url #2

--- next.config.js
+++ next.config.js
@@ -2,12 +2,12 @@ const withOffline = require('next-offline')
 
 const nextConfig = {
   target: 'serverless',
-  // transformManifest: manifest => ['/'].concat(manifest), // add the homepage to the cache
+  transformManifest: manifest => ['/'].concat(manifest), // add the homepage to the cache
   // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
   // turn on the SW in dev mode so that we can actually test it
   generateInDevMode: true,
   workboxOpts: {
-    swDest: 'static/service-worker.js',
+    swDest: 'service-worker.js',
     runtimeCaching: [
       {
         urlPattern: /^https?.*/,
