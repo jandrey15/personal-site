70644877eb503d7b4f562c1b4281e6713015823b
John Serrano <andrey_201015@hotmail.com>
2020-11-01 17:21:30 -0500

    Change url cover

--- components/Cover.js
+++ components/Cover.js
@@ -1,6 +1,9 @@
 const Cover = ({ children, cover, url }) => {
-  let urlM = url.replace('h_1000', 'h_425')
-  urlM = urlM.replace('w_1920', 'w_380')
+  let urlM
+  if (url) {
+    urlM = url.replace('h_1000', 'h_425')
+    urlM = urlM.replace('w_1920', 'w_380')
+  }
 
   return (
     <section id='Cover'>
