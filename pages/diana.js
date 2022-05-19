51c50129e8ba7654763ba3c3a1c06daa8ac119eb
John Serrano <andrey_201015@hotmail.com>
2022-05-19 14:45:04 -0500

    Add page diana for ever my love, my friend

---
+++ pages/diana.js
@@ -0,0 +1,33 @@
+
+const Diana = () => {
+  return (
+    <>
+      <div className='diana_container'>
+
+        <h4>Diana no quiero perder tu amistad, quiero que seamos amigos por siempre 😁</h4>
+        <p>By: Andrey Serrano ❤</p>
+      </div>
+      <style jsx>{`
+        .diana_container {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          text-align: center;
+          flex-direction: column;
+          height: 100vh;
+        }
+        p {
+          font-size: 21px;
+          font-weight: bold;
+          margin: 15px 0 0;
+        }
+        h4 {
+          font-size: 36px;
+          margin: 0;
+        }
+      `}</style>
+    </>
+  )
+}
+
+export default Diana
