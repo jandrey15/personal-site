4368bd97281f22990adf0029bae944c245f8c237
John Serrano <andrey_201015@hotmail.com>
2022-06-27 12:48:02 -0500

    Changes post diana

--- pages/diana.js
+++ pages/diana.js
@@ -6,6 +6,10 @@ const Diana = () => {
 
         <h4>Diana no quiero perder tu amistad, quiero que seamos amigos por siempre 😁 My friend forever.</h4>
         <h5>Entiendo si no te importa nuestra amistad, pero para mí vas a hacer siempre mi amiga eres una persona muy especial para mi yo siempre la voy a querer porque usted ayudo a cambiar mi vida y la aprecio no importa si me alejo o no volvemos hablar siempre la voy a llevar en mi ❤ la quiero mucho amiga mía, y lo siento de verdad por confundir las cosas y hacerla pasar por estas situaciones perdóneme diana, yo no quería tener esos sentimientos y sentir más allá de una amistad.</h5>
+        <time>19 de Mayo del 2022</time>
+
+        <h5>Diana algo que nunca voy a entender es porque nunca valoraste mi amistad, nunca me demostró que yo era importante para tu vida como un buen amigo siempre me he preguntado eso ¿porque siento que no me valoras? si yo lo único que hecho es quererte, apoyarte, ayudarte estar pendiente de tus cosas. Espero estar equivocado y estar juzgándote mal, también he sentido que debo permanecer un poco alejado y tengo otros sentimientos encontrados espero poder superar esto pronto. La llevo en mi corazón y siempre vas a ser importante para mí así ya no este tan presente en tu vida.</h5>
+        <time>27 de Junio del 2022</time>
         <p>By: Andrey Serrano ❤</p>
       </div>
       <style jsx>{`
@@ -15,8 +19,8 @@ const Diana = () => {
           align-items: center;
           text-align: center;
           flex-direction: column;
-          height: 100vh;
-          padding: 80px 5px;
+          height: 100%;
+          padding: 25px 5px;
         }
         p {
           font-size: 21px;
@@ -30,7 +34,7 @@ const Diana = () => {
         h5 {
           font-size: 26px;
           line-height: 29px;
-          margin: 25px 0 5px;
+          margin: 15px 0 5px;
           font-weight: 400;
         }
       `}</style>
