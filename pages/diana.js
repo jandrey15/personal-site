5562b06949d9a1843e07a15d4a2e9edde868e2b6
John Serrano <andrey_201015@hotmail.com>
2022-06-29 11:08:03 -0500

    Changes post diana#3

--- pages/diana.js
+++ pages/diana.js
@@ -10,6 +10,9 @@ const Diana = () => {
 
         <h5>Diana algo que nunca voy a entender es porque nunca valoraste mi amistad, nunca me demostró que yo era importante para tu vida como un buen amigo siempre me he preguntado eso ¿porque siento que no me valoras? si yo lo único que hecho es quererte, apoyarte, ayudarte estar pendiente de tus cosas. Espero estar equivocado y estar juzgándote mal, también he sentido que debo permanecer un poco alejado y tengo otros sentimientos encontrados espero poder superar esto pronto. La llevo en mi corazón y siempre vas a ser importante para mí así ya no este tan presente en tu vida.</h5>
         <time>27 de Junio del 2022</time>
+
+        <h5>Diana hace unos días soñé con vos, soñé que te daba un abrazo y vos no querias. Diana no sé qué está pasando, pero quiero que volvamos hacer amigos como antes no quiero dejar de ser tu amigo no quiero que por mis sentimientos nos alejemos, quiero que volvamos a ser amigos yo ya entendí que no va a pasar nada por eso yo me aleje porque me estaba haciendo daño necesitaba tiempo para mí para curar mis heridas espero me entiendas y no estes pensando que me aleje por rabia o porque ya no la aprecio como mi amiga, para mí siempre serás una persona muy especial. Yo no puedo seguir así como si no fuéramos nada como si nuestra amistad no valiera nada aparte tu eres mi hermana en la fe somos hermanos hijos de Dios no podemos estar así no sabe cuánto la quiero no sé porque la aprecio tanto quisiera saberlo y si es pasajero pues bueno no lo se. Todo esto te lo tengo que decir en persona, espero podamos hablar en persona pronto.</h5>
+        <time>29 de Junio del 2022</time>
         <p>By: Andrey Serrano ❤</p>
       </div>
       <style jsx>{`
